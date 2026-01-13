e4d9d32a624d4a24998e4c040bbc3cfe1b029f1a
Paul van Niekerk <stormwindpro1@gmail.com>
2026-01-13 15:50:55 +0200

    Final Reviewed Etch-A-Sketch TOP minigame completed

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-const gridElement = document.getElementById("grid");
-let number = 16;
+/********************/
+/*  Variable setup  */
+/********************/
 
-/***********************************************/
-/*  Grid element and child element parameters  */
-/***********************************************/
+let number = 16; //default
 
-let borderSize = 0;
+let borderSize = 0;  //Individual box element formating
 let marginSize = 0;
 
+const gridElement = document.getElementById("grid");
+const resetButton = document.querySelector("#reset-button");
+
+//Dynamic padding adjustment
 gridElement.style.padding = `${marginSize}px`;
 
-generateGrid();
+/********************/
+/*     functions    */
+/********************/
 
-function generateGrid(){
+function resetButtonEvent() {
+    let validInput = false;
 
-    for(let i = 0; i < number; i++){
+    while(validInput !== true){
+        number = parseInt(prompt("Please enter a value between 0 and 100 to determine gridsize", "50"));
+        if (number < 0 || number > 100) continue;
+
+        validInput = true;
+        gridElement.innerHTML = '';
+
+        generateGrid(number);
+        break;
+    }
+}
+
+function generateGrid(input){
+    //Individual box element dimention calculations
+    let dimentions = (
+                (
+                    gridElement.offsetWidth - 
+                    (gridElement.offsetWidth - gridElement.clientWidth) - 
+                    marginSize*2
+                )
+            ) / input;
+
+    let finalDimentions = dimentions - borderSize*2 - marginSize*2;
+
+    //Generate Grid
+    for(let i = 0; i < input; i++){
         const parent = document.createElement('div');
         parent.setAttribute("class", "parent");
         gridElement.appendChild(parent);
 
         for(let j = 0; j < number; j++){
             const box = document.createElement('div');
-            let dimentions = ((gridElement.offsetWidth - (gridElement.offsetWidth - gridElement.clientWidth) - marginSize*2)) / number;
-
-            box.style.width = `${dimentions - borderSize*2 - marginSize*2}px`;
-            box.style.height = `${dimentions - borderSize*2 - marginSize*2}px`;
+            
+            box.style.width = `${finalDimentions}px`;
+            box.style.height = `${finalDimentions}px`;
             box.style.margin = `${marginSize}px`;
             box.style.borderWidth = `${borderSize}px`;
 
@@ -38,33 +68,65 @@ function generateGrid(){
 }
 
 function mouseElementColistionEvent(element) {
-    console.log("Event triggered!");
-    //console.log(element.style.backgroundColor);
-    if(element.style.backgroundColor === ''){
-        let colour = getRandomColour();
-        console.log("Colour: " + colour);
-        element.style.backgroundColor = colour;
-    }
+    if(element.style.backgroundColor !== '') return;
+
+    let colour = getColourGoldenRation();
+    element.style.backgroundColor = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
 }
 
 /******************************************************************/
 /*  Fancy colour generating algorithm from Martin Leitner-Ankerl  */
 /******************************************************************/
 
-function getRandomColour(){
-    let characters = '0123456789ABCDEF';
-    let colour = '#';
-    for(let i = 0; i < 6; i++){
-        colour += characters[Math.floor(Math.random() * 16)];
-    }
+function getColourGoldenRation(){
+    goldenRatioConjugate = 0.618033988749895;
 
-    return colour;
+    let h = Math.random();
+
+    h += goldenRatioConjugate;
+    h %= 1;
+    return hsvToRgb(h, 0.85, 0.85);
 }
 
-// function hsvToRgb(h, s, v) {
-//     h_i = (h * 6).toFixed(0);
-// }
+function hsvToRgb(h, s, v) {
+    let r, g, b = 0;
+    let h_i = Math.trunc(h * 6);
+    let f = h * 6 - h_i;
+    let p = v * (1 - s);
+    let q = v * (1 - f * s);
+    let t = v * (1 - (1 - f) * s);
 
+    if(h_i === 0) [r, g, b] = [v, t, p];
+    if(h_i === 1) [r, g, b] = [q, v, p];
+    if(h_i === 2) [r, g, b] = [p, v, t];
+    if(h_i === 3) [r, g, b] = [p, q, v];
+    if(h_i === 4) [r, g, b] = [t, p, v];
+    if(h_i === 5) [r, g, b] = [v, p, q];
+
+    //console.log([Math.trunc(r * 256), Math.trunc(g * 256), Math.trunc(b * 256)]);
+
+    return [Math.trunc(r * 256), Math.trunc(g * 256), Math.trunc(b * 256)];
+}
+
+//reset button setup
+resetButton.addEventListener("click", () => resetButtonEvent());
+
+generateGrid(number); //Generate default setup
+
+
+/*********************************************/
+/*     Naive colour generating method        */
+/*********************************************/
+
+// function getRandomColour(){
+//     let characters = '0123456789ABCDEF';
+//     let colour = '#';
+//     for(let i = 0; i < 6; i++){
+//         colour += characters[Math.floor(Math.random() * 16)];
+//     }
+
+//     return colour;
+// }
 
 /*********************************************/
 /*  Non-functional percentage based spacing  */
